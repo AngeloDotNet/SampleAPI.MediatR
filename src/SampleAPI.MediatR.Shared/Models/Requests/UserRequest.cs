8e36446ee7fda7e2a8ea44dbdd3f0efe3c00415b
Angelo Pirola <angelo9871@gmail.com>
2022-12-06 14:47:20 +0100

    Added Shared project

---
+++ src/SampleAPI.MediatR.Shared/Models/Requests/UserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleAPI.MediatR.Shared.Models.Requests;
+
+public class UserRequest
+{
+    public int Id { get; set; }
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+}
