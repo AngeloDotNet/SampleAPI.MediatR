b2dadb6e635a797328ead64dc0bf5185a61614ac
Angelo Pirola <angelo9871@gmail.com>
2022-12-06 14:46:25 +0100

    Added WebApi project

---
+++ src/SampleAPI.MediatR/Controllers/UsersController.cs
@@ -0,0 +1,121 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.MediatR.BusinessLayer.Commands;
+using SampleAPI.MediatR.BusinessLayer.Queries;
+using SampleAPI.MediatR.Shared.Models.Requests;
+using System.Net.Mime;
+
+namespace SampleAPI.MediatR.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsersAsync()
+    {
+        try
+        {
+            var result = await mediator.Send(new GetUsersListQuery());
+
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserByIdAsync(int id)
+    {
+        try
+        {
+            var result = await mediator.Send(new GetUserByIdQuery(id));
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUserAsync(UserRequest request)
+    {
+        try
+        {
+            var result = await mediator.Send(new CreateUserCommand(request.Cognome, request.Nome, request.Telefono, request.Email));
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateUserAsync(UserRequest request)
+    {
+        try
+        {
+            var result = await mediator.Send(new UpdateUserCommand(request.Id, request.Cognome, request.Nome, request.Telefono, request.Email));
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteUserAsync(UserRequest request)
+    {
+        try
+        {
+            var result = await mediator.Send(new DeleteUserCommand(request.Id));
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ src/SampleAPI.MediatR/Extensions/ServiceExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAPI.MediatR.DataAccessLayer;
+using SampleAPI.MediatR.DataAccessLayer.Repository;
+using SampleAPI.MediatR.DataAccessLayer.Services;
+using SampleAPI.MediatR.DataAccessLayer.UnitOfWork;
+
+namespace SampleAPI.MediatR.Extensions;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddDependencyInjectionServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContextPool<DataDbContext>(optionsBuilder =>
+        //{
+        //    string connectionString = configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+        //    optionsBuilder.UseSqlite(connectionString);
+        //});
+
+        string connectionString = configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+
+        services.AddDbContext<DataDbContext>(options =>
+        options.UseSqlite(connectionString, b => b.MigrationsAssembly(typeof(DataDbContext).Assembly.FullName)));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
